bdcbba493ed9fed91508337de7db2632703fe0d7
Kayode Uthman <88583385+Utty007@users.noreply.github.com>
2023-10-15 06:04:46 -0700

    final edit, i think

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,78 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
-import React, { Fragment } from 'react';
+
+import React, { Fragment, useState, useEffect } from 'react';
+import PopUpCard from './UI/PopUpCard';
 import FrameOne from './Sections/Frame1';
-import './App.css'
+import './App.css';
 import NavBar from './Sections/NavBar';
 import Main from './Sections/Main';
 import Testimonials from './Sections/Testimonials';
 import CountdownTimer from './UI/Timer';
 
 const App = () => {
-  return <Fragment>
-    <CountdownTimer />
-    <NavBar />
-    <FrameOne />
-    <Main />
-    <Testimonials />
-  </Fragment>
-}
+  const [showPopUp, setShowPopUp] = useState(true);
+  const [currentDataIndex, setCurrentDataIndex] = useState(0);
 
-export default App
+  const data = [
+    { name: 'Mason', country: 'Canada' },
+    { name: 'Olivia', country: 'USA' },
+    { name: 'Liam', country: 'UK' },
+    { name: 'Isabella', country: 'Brazil' },
+    { name: 'Noah', country: 'Germany' },
+    { name: 'Ava', country: 'France' },
+    { name: 'Ethan', country: 'USA' },
+    { name: 'Sophia', country: 'Canada' },
+    { name: 'Lucas', country: 'UK' },
+    { name: 'Mia', country: 'Brazil' },
+    { name: 'William', country: 'Germany' },
+    { name: 'Emma', country: 'USA' },
+    { name: 'Henry', country: 'Canada' },
+    { name: 'Charlotte', country: 'UK' },
+    { name: 'Alexander', country: 'Brazil' },
+    { name: 'Sophie', country: 'France' },
+    { name: 'Luna', country: 'USA' },
+    { name: 'Liam', country: 'Canada' },
+    { name: 'Oliver', country: 'UK' },
+    { name: 'Aria', country: 'Brazil' },
+    { name: 'Ella', country: 'Germany' },
+    { name: 'Elijah', country: 'USA' },
+    { name: 'Benjamin', country: 'Canada' },
+    { name: 'Amelia', country: 'UK' },
+    { name: 'Harper', country: 'Brazil' },
+    { name: 'James', country: 'Germany' }
+  ]
+
+  const getRandomData = () => {
+    return data[currentDataIndex];
+  };
+
+  useEffect(() => {
+    const popUpInterval = setInterval(() => {
+      setShowPopUp(false);
+    }, 5000); // Hide the pop-up after 10 seconds
+
+    const displayInterval = setInterval(() => {
+      setCurrentDataIndex((prevIndex) => (prevIndex + 1) % data.length);
+      setShowPopUp(true);
+    }, 10000); // Display new data every 12 seconds (10 seconds hidden + 2 seconds displayed)
+
+    return () => {
+      clearInterval(popUpInterval);
+      clearInterval(displayInterval);
+    };
+  }, []);
+
+  return (
+    <Fragment>
+      {showPopUp && <PopUpCard {...getRandomData()} />}
+      <CountdownTimer />
+      <NavBar />
+      <FrameOne />
+      <Main />
+      <Testimonials />
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ src/UI/PopUpCard.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const PopUpCard = ({ name, country, onClose }) => (
+  <div
+    style={{
+      position: 'fixed',
+      top: '0',
+      left: '50%',
+      transform: 'translateX(-50%)',
+      backgroundColor: 'rgba(0, 0, 0, 0.7)',
+      color: 'white',
+      padding: '10px',
+      borderRadius: '5px',
+      fontSize: '16px',
+      zIndex: '999',
+      display: 'flex',
+      justifyContent: 'space-between',
+      width: '300px',
+    }}
+  >
+    <div>
+      {name} from {country} has gotten free access to the Program
+    </div>
+    <button onClick={onClose} style={{ backgroundColor: 'transparent', border: 'none', color: 'white', cursor: 'pointer' }}>
+      Close
+    </button>
+  </div>
+);
+
+export default PopUpCard
