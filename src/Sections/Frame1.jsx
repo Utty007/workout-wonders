ccc8d473f82dd464e185807cf7de5d0218fa85dc
Kayode Uthman <88583385+Utty007@users.noreply.github.com>
2023-10-15 09:59:31 -0700

    Again, finito, hopefully

--- src/Sections/Frame1.jsx
+++ src/Sections/Frame1.jsx
@@ -5,10 +5,12 @@ import VideoTwo from '../Asset/bgVidTwo.mp4';
 import VideoThree from '../Asset/bgVidThree.mp4';
 import Style from './Frame1.module.css'
 import Overlay from '../UI/Overlay';
+import audio from '../Asset/audio.mp3'
 
 const FrameOne = () => {
     return <div className={Style.FrameOne}>
         <Overlay />
+        <audio autoPlay={true} src={audio} loop={true} />
         <video src={VideoOne} autoPlay={true} loop={true} muted={true}/>
         <video src={VideoThree} autoPlay={true} className={Style.displayNone} loop={true} muted={true}/>
         <video src={VideoTwo} autoPlay={true} className={Style.displayOff} loop={true} muted={true} />
